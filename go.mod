66fb73d32ae9d5a2592ac75e62e4b391cd9e1b34
Daniel Akiva <dakiva@gmail.com>
2020-03-14 16:07:44 -0700

    Added module support. Reduce logging noise on api filter.

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/dakiva/goserv
+
+go 1.14
+
+require (
+	github.com/dakiva/dbx v1.2.0
+	github.com/dakiva/nogo v0.0.0-20160929181521-171fdc57df6c
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/emicklei/go-restful v2.12.0+incompatible
+	github.com/emicklei/go-restful-openapi v1.3.0
+	github.com/go-openapi/spec v0.19.7
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/lib/pq v1.3.0
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
+	github.com/stretchr/testify v1.5.1
+)
--- restful_logging_filter.go
+++ restful_logging_filter.go
@@ -25,55 +25,37 @@ import (
 
 const (
 	// DefaultRequestLogFormat format
-	DefaultRequestLogFormat = "[%s %s] request\n"
-	// DefaultVerboseRequestLogFormat format
-	DefaultVerboseRequestLogFormat = "[%s %s] request data\n\trem_ip='%s'\n\treceived_cookies='%s'\n\treferer='%s'\n\tuser_agent='%s'\n\tcontent_length='%d'\n\tcontent_type='%s'\n\tform='%s'\nrequest headers:\n%s\n"
+	DefaultRequestLogFormat = "[%s %s] request data\n\trem_ip='%s'\n\treceived_cookies='%s'\n\treferer='%s'\n\tuser_agent='%s'\n\tcontent_length='%d'\n\tcontent_type='%s'\n\tform='%s'\nrequest headers:\n%s\n"
 	// DefaultResponseLogFormat format
-	DefaultResponseLogFormat = "[%s %s] response\n\tstatus='%d'\n\tresponse_time='%s'\n"
-	// DefaultVerboseResponseLogFormat format
-	DefaultVerboseResponseLogFormat = "[%s %s] response headers:\n%s\n"
+	DefaultResponseLogFormat = "[%s %s] response\n\tstatus=%d\n\ttime=%s\nheaders:\n%s\n"
 
-	timeRequestAttribute = "time_attr"
+	timeRequestAttribute = "request_time_attr"
 )
 
 // RestfulLoggingFilter is middleware that logs restful requests and response payloads
 type RestfulLoggingFilter struct {
-	logger                   *logging.Logger
-	requestLogFormat         string
-	verboseRequestLogFormat  string
-	responseLogFormat        string
-	verboseResponseLogFormat string
+	logger            *logging.Logger
+	requestLogFormat  string
+	responseLogFormat string
 }
 
 // NewRestfulLoggingFilter initializes a new logging filter instance
 func NewRestfulLoggingFilter(logger *logging.Logger) *RestfulLoggingFilter {
 	return &RestfulLoggingFilter{
-		logger:                   logger,
-		requestLogFormat:         DefaultRequestLogFormat,
-		verboseRequestLogFormat:  DefaultVerboseRequestLogFormat,
-		responseLogFormat:        DefaultResponseLogFormat,
-		verboseResponseLogFormat: DefaultVerboseResponseLogFormat,
+		logger:            logger,
+		requestLogFormat:  DefaultRequestLogFormat,
+		responseLogFormat: DefaultResponseLogFormat,
 	}
 }
 
-// SetRequestLogFormat sets the request log format
+// SetRequestLogFormat sets the verbose request log format
 func (r *RestfulLoggingFilter) SetRequestLogFormat(format string) {
 	r.requestLogFormat = format
 }
 
-// SetVerboseRequestLogFormat sets the verbose request log format
-func (r *RestfulLoggingFilter) SetVerboseRequestLogFormat(format string) {
-	r.verboseRequestLogFormat = format
-}
-
-// SetResponseLogFormat sets the response log format
+// SetResponseLogFormat sets the verbose response log format
 func (r *RestfulLoggingFilter) SetResponseLogFormat(format string) {
 	r.responseLogFormat = format
-}
-
-// SetVerboseResponseLogFormat sets the verbose response log format
-func (r *RestfulLoggingFilter) SetVerboseResponseLogFormat(format string) {
-	r.verboseResponseLogFormat = format
 }
 
 // Filter is a filter function that logs before and after processing the request
@@ -85,9 +67,6 @@ func (r *RestfulLoggingFilter) Filter(request *restful.Request, response *restfu
 		chain.ProcessFilter(request, response)
 		return
 	}
-	if r.logger.IsEnabledFor(logging.INFO) {
-		r.logger.Infof(r.requestLogFormat, request.Request.Method, request.Request.URL)
-	}
 	if r.logger.IsEnabledFor(logging.DEBUG) {
 		headerStr := ""
 		for k, v := range request.Request.Header {
@@ -97,7 +76,7 @@ func (r *RestfulLoggingFilter) Filter(request *restful.Request, response *restfu
 				headerStr = headerStr + fmt.Sprintf("\t%v:\t%v\n", k, v)
 			}
 		}
-		r.logger.Debugf(r.verboseRequestLogFormat, request.Request.Method, request.Request.URL, request.HeaderParameter("X-Forwarded-For"), request.Request.Cookies(), request.Request.Referer(), request.Request.UserAgent(), request.Request.ContentLength, request.HeaderParameter("Content-Type"), request.Request.Form, headerStr)
+		r.logger.Debugf(r.requestLogFormat, request.Request.Method, request.Request.URL, request.HeaderParameter("X-Forwarded-For"), request.Request.Cookies(), request.Request.Referer(), request.Request.UserAgent(), request.Request.ContentLength, request.HeaderParameter("Content-Type"), request.Request.Form, headerStr)
 	}
 
 	chain.ProcessFilter(request, response)
@@ -105,17 +84,12 @@ func (r *RestfulLoggingFilter) Filter(request *restful.Request, response *restfu
 		if r.logger.IsEnabledFor(logging.ERROR) {
 			r.logger.Errorf(r.responseLogFormat, request.Request.Method, request.Request.URL, response.StatusCode(), time.Now().Sub(start))
 		}
-	} else {
-		if r.logger.IsEnabledFor(logging.INFO) {
-			r.logger.Infof(r.responseLogFormat, request.Request.Method, request.Request.URL, response.StatusCode(), time.Now().Sub(start))
-		}
-	}
-	if r.logger.IsEnabledFor(logging.DEBUG) {
+	} else if r.logger.IsEnabledFor(logging.DEBUG) {
 		headerStr := ""
 		for k, v := range response.Header() {
 			headerStr = headerStr + fmt.Sprintf("\t%v:\t%v\n", k, v)
 		}
-		r.logger.Debugf(r.verboseResponseLogFormat, request.Request.Method, request.Request.URL, headerStr)
+		r.logger.Debugf(r.responseLogFormat, request.Request.Method, request.Request.URL, response.StatusCode(), time.Now().Sub(start), headerStr)
 	}
 }
 
